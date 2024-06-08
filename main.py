fde9b8745221fbe190f67a26daa87fce762c646b
kharl112 <kharl112>
2024-06-08 13:56:55 +0000

    batman

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import urllib.request
+from urllib.error import HTTPError, URLError
+
+import xmltodict
+import json
+
+import sys
+
+
+
+def get_url_contents(url):
+  try:
+    res = urllib.request.urlopen(url)
+    content = res.read()
+
+    return content
+
+
+  except HTTPError as e:
+    print(f"Eror url {url}: {e.code}: {e.reason}")
+  except URLError as e:
+    print(f"Eror url {url}: {e.reason}")
+  except Exception as e:
+    print(f"Eror url {url}: {e}")
+
+    return False 
+
+
+xml_data = get_url_contents('https://www.nobleprog.com.ph/sitemap.xml')
+
+if xml_data is not False:
+  xml_dict = xmltodict.parse(xml_data)
+
+  for row in xml_dict.get('urlset').get('url'):
+    loc = row.get('loc')
+
+    res = get_url_contents(loc)
+    if res is not False:
+      print(loc, ' okay') 
+
+else:
+  sys.exit(1)
+
+
+
+
+
+
