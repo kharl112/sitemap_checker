8ddff85a6712e3d355df85acf820fdb49ab8c056
root <kharl.dms112@gmail.com>
2024-09-10 14:07:24 +0000

    fixed none types

--- main.py
+++ main.py
@@ -19,21 +19,23 @@ def get_url_contents(url):
 
   except HTTPError as e:
     print(f"Eror url {url}: {e.code}: {e.reason}")
+    return False
   except URLError as e:
     print(f"Eror url {url}: {e.reason}")
+    return False
   except Exception as e:
     print(f"Eror url {url}: {e}")
-
     return False 
 
 
-xml_data = get_url_contents('https://www.nobleprog.com.ph/sitemap.xml')
+xml_data = get_url_contents('https://example.com/sitemap.xml')
 
 if xml_data is not False:
   xml_dict = xmltodict.parse(xml_data)
 
   for row in xml_dict.get('urlset').get('url'):
     loc = row.get('loc')
+    print(loc)
 
     res = get_url_contents(loc)
     if res is False:
